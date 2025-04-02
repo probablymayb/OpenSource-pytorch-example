5cd4add800d852ea8a29c9de31cb714bbafa937d
이민영 <ljylmy1@gmail.com>
2025-04-02 06:39:31 +0000

    commit test

--- mnist/main.py
+++ mnist/main.py
@@ -31,7 +31,7 @@ class Net(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
-
+#test
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
